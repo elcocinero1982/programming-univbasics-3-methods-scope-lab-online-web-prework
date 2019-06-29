67c87be27b9a8b6b8d78825b559ed51bf2ed8d81
jose luis <cortezluisjose@gmail.com>
2019-06-29 01:31:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,22 @@
 def mario
+ 
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "It's-a-me, Mario!"
 end
+
+
 
 def toadstool
   puts status
 end
+def link
+  
+  puts "It's Dangerous To Go Alone! Take This.'"
+end
+def any_prhase
+  local_variable = "Do A Barrel Roll!"
+  puts local_variable
+ 
+end
+
+  
